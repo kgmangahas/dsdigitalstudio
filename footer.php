3ed697a19cffdccc2749702b1e93b74589b87a6b
Kate <kateline.mangahas@gmail.com>
2017-09-08 18:50:50 +0800

    Added footer.

--- footer.php
+++ footer.php
@@ -15,6 +15,29 @@
 
 <?php wp_footer(); ?>
 
+<div class="wrapper ">
+
+			<div class="footer">				
+
+				
+				<span class="icon icon--ds footer__sign">					
+				</span>						
+
+				<p class="footer__copy">&copy; 2017 DS Digital Studio</p>
+
+				<div class="footer__social-media">
+					<a href="#" class="social-icons__icon social-icons__icon--footer"><span class="icon icon--fb"></span></a>
+				          <a href="#" class="social-icons__icon social-icons__icon--footer"><span class="icon icon--twitter"></span></a>
+				          <a href="#" class="social-icons__icon social-icons__icon--footer"><span class="icon icon--instagram"></span></a>
+				          <a href="#" class="social-icons__icon social-icons__icon--footer"><span class="icon icon--tumbler"></span></a>
+							
+					
+				</div>
+			
+				
+			</div>
+		</div>
+
 <!-- build:js assets/scripts/App.js -->  
 		<script src="<?php bloginfo('template_directory');?>/assets/scripts/App-4ba8e52f1d.js"></script>
 		<!-- endbuild -->
